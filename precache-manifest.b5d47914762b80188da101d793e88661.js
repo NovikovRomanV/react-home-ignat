3a542988ba21f318c484e65efa2224169314f906
NovikovRomanV <rimm19845@gmail.com>
2020-08-23 00:02:30 +0300

    Updates

---
+++ precache-manifest.b5d47914762b80188da101d793e88661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761167cde5f2f0707cac83d3eafb0738",
+    "url": "/react-home-ignat/index.html"
+  },
+  {
+    "revision": "d193d6e9511f89082324",
+    "url": "/react-home-ignat/static/css/main.59bdf181.chunk.css"
+  },
+  {
+    "revision": "5587ca2fb7b9b743f5fa",
+    "url": "/react-home-ignat/static/js/2.600d2dd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-home-ignat/static/js/2.600d2dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d193d6e9511f89082324",
+    "url": "/react-home-ignat/static/js/main.697a5b84.chunk.js"
+  },
+  {
+    "revision": "186e495d50b8794e3fe8",
+    "url": "/react-home-ignat/static/js/runtime-main.f9e834fb.js"
+  }
+]);
